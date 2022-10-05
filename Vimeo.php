096b7013dca44faa77c6515b46c1a5d6a1401415
Christian Widauer <9198@htl.rennweg.at>
2022-10-05 12:29:32 +0200

    vimeo und youtube files fertig??

--- Vimeo.php
+++ Vimeo.php
@@ -1,6 +1,36 @@
 <?php
 
-class Vimeo
+class Vimeo implements InterfaceVideo
 {
+    private string $name;
+    private string $source;
+    private string $htmlCode;
 
+    /**
+     * @param string $name
+     * @param string $source
+     * @param string $htmlCode
+     */
+    public function __construct(string $name, string $source, string $htmlCode)
+    {
+        $this->name = $name;
+        $this->source = $source;
+        $this->htmlCode = $htmlCode;
+    }
+
+
+    public function getName()
+    {
+        // TODO: Implement getName() method.
+    }
+
+    public function getSource()
+    {
+        // TODO: Implement getSource() method.
+    }
+
+    public function getHtmlCode()
+    {
+        // TODO: Implement getHtmlCode() method.
+    }
 }
--- Youtube.php
+++ Youtube.php
@@ -1,6 +1,35 @@
 <?php
 
-class Youtube
+class Youtube implements InterfaceVideo
 {
+    private string $name;
+    private string $source;
+    private string $htmlCode;
 
+    /**
+     * @param string $name
+     * @param string $source
+     * @param string $htmlCode
+     */
+    public function __construct(string $name, string $source, string $htmlCode)
+    {
+        $this->name = $name;
+        $this->source = $source;
+        $this->htmlCode = $htmlCode;
+    }
+
+    public function getName()
+    {
+        // TODO: Implement getName() method.
+    }
+
+    public function getSource()
+    {
+        // TODO: Implement getSource() method.
+    }
+
+    public function getHtmlCode()
+    {
+        // TODO: Implement getHtmlCode() method.
+    }
 }
