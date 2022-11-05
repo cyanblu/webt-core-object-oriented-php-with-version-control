6877e72fcac0e9426d12f2add8f17bad80484478
Moritz Liedl <9186@htl.rennweg.at>
2022-11-05 18:18:29 +0100

    sass funktioniert bei allen

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP mit Version Control</title>
-    <link rel="stylesheet" type="text/css" href="css\responisve.css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </head>
@@ -14,15 +14,13 @@
         require_once "InterfaceVideo.php";
         require_once "Youtube.php";
         require_once "Vimeo.php";
-        $videos[] = new Youtube('SURVIVE Trailer German Deutsch (2022)', 'https://www.youtube.com/embed/xe_2QMZ7OKs', '<div class="col"><iframe src="https://www.youtube.com/embed/xe_2QMZ7OKs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>', 'Laufzeit: 2:15');
-        $videos[] = new Vimeo('', '', '<div class="col"><iframe src="https://player.vimeo.com/video/202696600?h=c3a851aff4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>', '"Laufzeit: 2:15"');
+        $videos[] = new Youtube('SURVIVE Trailer German Deutsch (2022)', 'https://www.youtube.com/embed/xe_2QMZ7OKs', '<div class="col"><iframe src="https://www.youtube.com/embed/xe_2QMZ7OKs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>');
+        $videos[] = new Vimeo('', '', '<div class="col"><iframe src="https://player.vimeo.com/video/202696600?h=c3a851aff4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>');
         echo '<div class="container text-center">
         <div class="row row-cols-4">';
         for ($i = 1; $i <=10; $i++) {
             echo $videos[0]->getHtmlCode();
-            echo $videos[0]->getTime();
             echo $videos[1]->getHtmlCode();
-            echo $videos[1]->getTime();
         }
         echo '</div></div>';
     ?>
